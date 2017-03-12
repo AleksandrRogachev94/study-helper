07491c31e263c852c1d050f068a5693e229d400b
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-03-12 19:37:43 -0400

    add helper method to lessons index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -7,4 +8,11 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+
+    def set_user
+      @user = User.find_by(id: params[:id])
+      return redirect_to root_path, alert: "The User doesn't exist" if !@user
+    end
 end
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,10 @@
 module LessonsHelper
+  def list_index_title(user)
+    if user == current_user
+      "Your Lessons"
+    else
+      "Lessons of #{user.email}"
+    end
+
+  end
 end
