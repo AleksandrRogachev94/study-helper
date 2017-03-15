5b2ae5d06e0e58f55e30b86ea8015ef6aec83b05
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-03-14 20:13:59 -0400

    add users index styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,17 @@ module UsersHelper
       render 'requests/form'
     end
   end
+
+  def user_class_bs(user)
+    result = "list-group-item list-group-item-action"
+
+    if current_user.students.include?(user) && current_user.teachers.include?(user)
+      result << " list-group-item-success"
+    elsif current_user.students.include?(user) #your student
+      result << " list-group-item-purple"
+    elsif current_user.teachers.include?(user) #your teacher
+      result << " list-group-item-info"
+    end
+    result
+  end
 end
