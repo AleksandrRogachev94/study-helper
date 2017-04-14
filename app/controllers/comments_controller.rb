13a7da4a43ed0f8f34f1f18bf5797106fce0df29
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-14 17:33:32 -0400

    fix partials for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,4 @@
+#
 class CommentsController < ApplicationController
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  validates :content, presence: true
 end
