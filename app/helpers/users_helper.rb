37902c12e6f0c5539c6e1583a7b47ad52bda5837
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-17 21:38:13 -0400

    fix and style users index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,16 +9,39 @@ module UsersHelper
     end
   end
 
-  def user_class_bs(user)
-    result = "list-group-item list-group-item-action"
+  def link_to_user(user)
+    css_class = "list-group-item list-group-item-action"
+    appearance = user.appearance
 
     if current_user.students.include?(user) && current_user.teachers.include?(user)
-      result << " list-group-item-success"
+      css_class << " list-group-item-success"
+      appearance << " (your student and teacher)"
     elsif current_user.students.include?(user) #your student
-      result << " list-group-item-purple"
+      css_class << " list-group-item-purple"
+      appearance << " (your student)"
     elsif current_user.teachers.include?(user) #your teacher
-      result << " list-group-item-info"
+      css_class << " list-group-item-info"
+      appearance << " (your teacher)"
     end
-    result
+
+    link_to appearance, user_path(user), class: css_class
+  end
+
+  def link_to_user_popular(user)
+    css_class = "list-group-item list-group-item-action"
+    appearance = user.appearance +  " - " + pluralize(user.students_count, "student")
+
+    if current_user.students.include?(user) && current_user.teachers.include?(user)
+      css_class << " list-group-item-success"
+      appearance << " (your student and teacher)"
+    elsif current_user.students.include?(user) #your student
+      css_class << " list-group-item-purple"
+      appearance << " (your student)"
+    elsif current_user.teachers.include?(user) #your teacher
+      css_class << " list-group-item-info"
+      appearance << " (your teacher)"
+    end
+
+    link_to appearance, user_path(user), class: css_class
   end
 end
